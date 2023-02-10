f2a22de9bd472b7b4877a8bdd9bde4d5b9d40472
thedayingalaxy <thedayingalaxy@gmail.com>
2023-02-10 23:00:28 +0000

    hello yoda

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,13 @@
 import { View, Text, SafeAreaView, Image} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 import{useNavigation} from '@react-navigation/native';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+  } from "react-native-heroicons/outline";
+import { TextInput } from 'react-native';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -23,15 +30,18 @@ const HomeScreen = () => {
               uri: 'https://i.insider.com/5e32f2a324306a19834af322?width=750&format=jpeg&auto=webp',
             }}
             className= "h-7 w-7 bg-gray-300 p-4 rounded-full"/>
-          <View>
+          <View className= 'flex-1'>
             <Text classNAme="font-bold text-gray-400 text-xs">
               Deliver Now!
             </Text>
             <Text className="font-bold text-xl">
               Current Location
+              <ChevronDownIcon size={20} color="#00CCBB"/>
             </Text>
           </View>
+          <UserIcon size={35} color="#00CCBB" />
         </View>
+
 
       </Text>
     </SafeAreaView>
