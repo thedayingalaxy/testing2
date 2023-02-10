3d303e2a521fb24b81b0c7335889750ad5e39ac8
thedayingalaxy <thedayingalaxy@gmail.com>
2023-02-10 19:21:30 +0000

    current location and user profile

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, Image} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 import{useNavigation} from '@react-navigation/native';
 
@@ -14,10 +14,27 @@ const HomeScreen = () => {
 
 
   return (
-    <View>
-      <Text className = "text-red-500" >HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <Text className = "text-red-500" >
+        {/* Header */}
+        <View className = "flex-row pb-3 items-centre mx-4 space-x-2">
+          <Image
+            source ={{
+              uri: 'https://i.insider.com/5e32f2a324306a19834af322?width=750&format=jpeg&auto=webp',
+            }}
+            className= "h-7 w-7 bg-gray-300 p-4 rounded-full"/>
+          <View>
+            <Text classNAme="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+            </Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
