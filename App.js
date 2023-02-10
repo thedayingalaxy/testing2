11290e27e291e9183fd121d81f84d8e5c7efd1b4
thedayingalaxy <thedayingalaxy@gmail.com>
2023-02-10 02:06:43 +0000

    stages

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500">Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
