41fe253855213aed49d4e7571c3d57c497a02d89
thedayingalaxy <thedayingalaxy@gmail.com>
2023-02-10 18:18:16 +0000

    headershown--false

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import{useNavigation} from '@react-navigation/native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() =>{
+    navigation.setOptions({
+    headerShown: false,
+    });
+  }, []);
+
+
+
   return (
     <View>
       <Text className = "text-red-500" >HomeScreen</Text>
